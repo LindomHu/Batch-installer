f35b8a2d67e192873ba645ec5793842d7619843f
LindomHu <44762133+LindomHu@users.noreply.github.com>
2022-01-10 21:01:42 +0800

    Python [Function optimization] on 10 Jan 2022

--- DeviceManager.py
+++ DeviceManager.py
@@ -27,7 +27,7 @@ class DeviceManager():
             print(e)
 
         self.root = Tk()
-        self.root.geometry('1200x450')
+        self.root.geometry('1200x480')
         self.root.title("DeviceManager")
         self.logcat_path = StringVar()
         self.logcat_path.set("D:\\DeviceManager\\LogcatLog")
@@ -107,7 +107,7 @@ class DeviceManager():
 
         # 刷新adb
         button_refresh = Button(self.root, text="刷新adb", bg="LightSteelBlue", command=lambda: self.refresh_adb())
-        button_refresh.grid(row=0, column=8)
+        button_refresh.grid(row=0, column=7)
         # 关闭adb
         button_close_adb = Button(self.root, text="关闭adb", bg="DarkGray",command=self.close_adb)
         button_close_adb.grid(row=0, column=9)
@@ -210,9 +210,13 @@ class DeviceManager():
             self.root.grid_columnconfigure((0,3,5), minsize=20)
 
     def draw_label_text(self):
-        label_text = tk.Label(self.root, text="提示：点击[关闭adb]/[停止抓logcat日志]之后需要重新点击[刷新adb]",
+        label_text1 = tk.Label(self.root, text="提示：点击[关闭adb]/[停止抓logcat日志]之后需要重新点击[刷新adb]",
                               foreground="Yellow",background="Gray",)
-        label_text.place(x=605,y=75)
+        label_text1.place(x=683,y=67)
+
+        label_text2 = tk.Label(self.root, text="如果在Mac上运行，日志保存路径需要手动填写，默认的会找不到",
+                              foreground="Yellow",background="Gray",)
+        label_text2.place(x=683,y=90)
 
     def mainloop(self):
         self.draw_log_path()
