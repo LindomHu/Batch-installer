d12b944cb4da6811556d9647c7330f692125fec5
LindomHu <44762133+LindomHu@users.noreply.github.com>
2022-01-17 19:03:15 +0800

    Python [Resolve Refresh adb of question]

--- InstallApk.py
+++ InstallApk.py
@@ -17,56 +17,62 @@ import threading
 import platform
 
 
-
-
-class InstallItem:
+class InstallApk:
     def __init__(self,root,device,index,DeviceManager):
         self.DeviceManager = DeviceManager
-        self.device  = device
-        self.root = root
+        self.device = device
+        # self.device = self.DeviceManager.
+
         # 选择Apk
         self.entry_default = StringVar()
-        self.entry_default.set("请选择一个Apk文件")
+        self.entry_default.set("请选择一个待安装的Apk文件或输入Apk文件的路径")
 
+        self.root = root
         self.lab_device = tk.Label(self.root, text=device)
-        self.lab_device.grid(row=index+1,column=1,stick='w')
+        self.lab_device.grid(row=index+2,column=1,stick='w')
 
         self.entry_apk = Entry(self.root, width=45, textvariable=self.entry_default)
-        self.entry_apk.grid(row=index+1,column=2,stick='w')
+        self.entry_apk.grid(row=index+2,column=2,stick='w')
 
-        self.select_apk_button = tk.Button(self.root, text ="选择Apk", command = self.thread_choose_file)
-        self.select_apk_button.grid(row=index+1,column=5,stick='w')
+        self.button_select_apk = tk.Button(self.root, text ="选择Apk", command = self.thread_choose_file)
+        self.button_select_apk.grid(row=index+2,column=5,stick='w')
 
         # 安装
         self.button_install = Button(self.root, text="安装", bg="LightSteelBlue", command=self.thread_install_apk)
-        self.button_install.grid(row=index+1,column=4,stick='w')
+        self.button_install.grid(row=index+2,column=4,stick='w')
 
         # 卸载
         self.button_uninstall = Button(self.root, text="卸载", command=self.thread_uninstall_apk)
-        self.button_uninstall.grid(row=index+1, column=3, stick='w')
+        self.button_uninstall.grid(row=index+2, column=3, stick='w')
 
         # 日志
         self.button_logcat_c = Button(self.root,text="删logcat日志",command=self.logcat_c)
-        self.button_logcat_c.grid(row=index+1,column=7,stick='w')
+        self.button_logcat_c.grid(row=index+2,column=7,stick='w')
 
         self.button_logcat_log = Button(self.root,text="抓logcat日志",bg="LightSteelBlue", command=self.logcat_log)
-        self.button_logcat_log.grid(row=index+1,column=8,stick='w')
+        self.button_logcat_log.grid(row=index+2,column=8,stick='w')
 
         self.button_delete_GCloudlog = Button(self.root,text="删GCloud日志",command=self.delete_GCloudlog)
-        self.button_delete_GCloudlog.grid(row=index+1,column=9,stick='w')
+        self.button_delete_GCloudlog.grid(row=index+2,column=9,stick='w')
 
         self.button_pull_GCloudlog = Button(self.root,text="拉GCloud日志",bg="LightSteelBlue", command=self.pull_GCloudlog)
-        self.button_pull_GCloudlog.grid(row=index+1,column=10,stick='w')
+        self.button_pull_GCloudlog.grid(row=index+2,column=10,stick='w')
 
         self.button_delete_Corelog = Button(self.root, text="删GCloudCore日志", command=self.delete_Corelog)
-        self.button_delete_Corelog.grid(row=index+1, column=11, stick='w')
+        self.button_delete_Corelog.grid(row=index+2, column=11, stick='w')
 
         self.button_pull_Corelog = Button(self.root, text="拉GCloudCore日志", bg="LightSteelBlue", command=self.pull_Corelog)
-        self.button_pull_Corelog.grid(row=index+1, column=12, stick='w')
+        self.button_pull_Corelog.grid(row=index+2, column=12, stick='w')
 
+        self.button_delete_GVoicelog = Button(self.root, text="删GVoice日志", command=self.delete_GVoicelog)
+        self.button_delete_GVoicelog.grid(row=index+2, column=13, stick='w')
+
+        self.button_pull_GVoicelog = Button(self.root, text="拉GVoice日志", bg="LightSteelBlue", command=self.pull_GVoicelog)
+        self.button_pull_GVoicelog.grid(row=index+2, column=14, stick='w')
 
     def destroy(self):
-        self.select_apk_button.destroy()
+        self.entry_apk.destroy()
+        self.button_select_apk.destroy()
         self.button_install.destroy()
         self.button_uninstall.destroy()
         self.button_logcat_c.destroy()
@@ -75,6 +81,8 @@ class InstallItem:
         self.button_pull_GCloudlog.destroy()
         self.button_delete_Corelog.destroy()
         self.button_pull_Corelog.destroy()
+        self.button_delete_GVoicelog.destroy()
+        self.button_pull_GVoicelog.destroy()
         self.entry_apk.destroy()
         self.lab_device.destroy()
 
@@ -107,12 +115,11 @@ class InstallItem:
             device_name = self.device
 
         platform_system = platform.system()
-        Win = "Windows"
-        if platform_system == Win:
+        if platform_system == "Windows":
             computer_copy_path = self.DeviceManager.get_GCloudlog_path() +'\\' + device_name+"\\" # 本地的路径，存在指定的文件夹再加上设备名作为区分
         else:
             computer_copy_path = self.DeviceManager.get_GCloudlog_path() +'/' + device_name+"/" # 本地的路径，存在指定的文件夹再加上设备名作为区分
-        print("GCloud日志拉取到本地的路径：%s"%computer_copy_path)
+        print("\033[32;1mGCloud日志拉取到本地的路径：%s\033[0m"%computer_copy_path)
 
         if not os.path.exists(computer_copy_path):
             os.makedirs(computer_copy_path)
@@ -148,18 +155,57 @@ class InstallItem:
             device_name = self.device
 
         platform_system = platform.system()
-        Win = "Windows"
-        if platform_system == Win:
+        if platform_system == "Windows":
             computer_copy_path = self.DeviceManager.get_Corelog_path() +'\\' + device_name+"\\" # 本地的路径，存在指定的文件夹再加上设备名作为区分
         else:
             computer_copy_path = self.DeviceManager.get_Corelog_path() +'/' + device_name+"/" # 本地的路径，存在指定的文件夹再加上设备名作为区分
-        print("GCloudCore日志拉取到本地的路径：%s"%computer_copy_path)
+        print("\033[32;1mGCloudCore日志拉取到本地的路径：%s\033[0m"%computer_copy_path)
 
         if not os.path.exists(computer_copy_path):
             os.makedirs(computer_copy_path)
 
-        cmd_pull = 'adb -s {0} pull {1} {2}'.format(self.device, device_Corelog_path, computer_copy_path)
-        os.system(cmd_pull)
+        cmd_pull2 = 'adb -s {0} pull {1} {2}'.format(self.device, device_Corelog_path, computer_copy_path)
+        os.system(cmd_pull2)
+
+    def delete_GVoicelog(self):
+        print("device",self.device)
+        # if "emulator" in self.device or "127.0.0.1" in self.device:#如果是模拟器
+        device_GVoicelog_path = "/sdcard/Android/data/com.tencent.itop.example/cache/GCloudSDKLog/GVoice"
+        # else:# 如果是手机
+        #     device_log_path = "/storage/emulated/0/Android/data/com.tencent.itop.example/cache/GCloudSDKLog/GCloud"
+        if ':' in self.device:
+            device_name = self.device.replace(':', '-')  # 这里用来处理模拟器多开，冒号在路径名中无法使用，所以替换一下
+        else:
+            device_name = self.device
+
+        cmd_delete3 = 'adb -s {0} shell rm -rf {1}'.format(self.device, device_GVoicelog_path)
+        print(cmd_delete3)
+        os.system(cmd_delete3)
+
+
+    def pull_GVoicelog(self):
+        print("device",self.device)
+        # if "emulator" in self.device or "127.0.0.1" in self.device:#如果是模拟器
+        device_GVoicelog_path = "/sdcard/Android/data/com.tencent.itop.example/cache/GCloudSDKLog/GVoice"
+        # else:# 如果是手机
+        #     device_log_path = "/storage/emulated/0/Android/data/com.tencent.itop.example/cache/GCloudSDKLog/GCloud"
+        if ':' in self.device:
+            device_name = self.device.replace(':', '-')  # 这里用来处理模拟器多开，冒号在路径名中无法使用，所以替换一下
+        else:
+            device_name = self.device
+
+        platform_system = platform.system()
+        if platform_system == "Windows":
+            computer_copy_path = self.DeviceManager.get_GVoicelog_path() +'\\' + device_name+"\\" # 本地的路径，存在指定的文件夹再加上设备名作为区分
+        else:
+            computer_copy_path = self.DeviceManager.get_GVoicelog_path() +'/' + device_name+"/" # 本地的路径，存在指定的文件夹再加上设备名作为区分
+        print("\033[32;1mGVoice日志拉取到本地的路径：%s\033[0m"%computer_copy_path)
+
+        if not os.path.exists(computer_copy_path):
+            os.makedirs(computer_copy_path)
+
+        cmd_pull3 = 'adb -s {0} pull {1} {2}'.format(self.device, device_GVoicelog_path, computer_copy_path)
+        os.system(cmd_pull3)
 
     def logcat_c(self):
         print("device",self.device)
@@ -167,7 +213,6 @@ class InstallItem:
             device_name = self.device.replace(':', '-')  # 这里用来处理模拟器多开，冒号在路径名中无法使用，所以替换一下
         else:
             device_name = self.device
-
 
         cmd_logcat_c = 'adb -s {0} logcat -c'.format(self.device)
         print(cmd_logcat_c)
@@ -181,12 +226,11 @@ class InstallItem:
             device_name = self.device
 
         platform_system = platform.system()
-        Win = "Windows"
-        if platform_system == Win:
+        if platform_system == "Windows":
             computer_save_path = self.DeviceManager.get_logcat_path() +'\\' + device_name+"\\" # 本地的路径，存在指定的文件夹再加上设备名作为区分
         else:
             computer_save_path = self.DeviceManager.get_logcat_path() +'/' + device_name+"/" # 本地的路径，存在指定的文件夹再加上设备名作为区分
-        print("logcat日志保存到本地的路径：%s"%computer_save_path)
+        print("\033[32;1mlogcat日志保存到本地的路径：%s[0m"%computer_save_path)
 
         if not os.path.exists(computer_save_path):
             os.makedirs(computer_save_path)
